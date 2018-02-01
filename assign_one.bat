4e11300f6f7336631656f6262b7d689aa165e6bb
erikn <erik.nystrom87@gmail.com>
2018-02-01 18:18:45 -0500

    fix batch

--- assign_one.bat
+++ assign_one.bat
@@ -1,7 +1,7 @@
 javac -d classes -classpath classes;. model\*.java
-javac -classpath classes;. TestAssgn1.java
+javac -classpath classes;. TestAssign1.java
 
 echo "Classes for assignment one compiled"
 pause
 
-java -cp mysql-connector-java-5.1.7-bin.jar;classes;. TestAssgn1
+java -cp mysql-connector-java-5.1.7-bin.jar;classes;. TestAssign1
