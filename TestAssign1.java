05295a4df745e5f04010ed7d8797be805392fdb5
Erik Nystrom <erik.nystrom87@gmail.com>
2018-02-06 16:04:33 -0500

    Fixed command line usage, breaks IDE? Updated SQL database Fixed Older/Younger query, WHERES has no spaces

--- TestAssign1.java
+++ TestAssign1.java
@@ -8,7 +8,7 @@ import model.PatronCollection;
 import model.Patron;
 
 public class TestAssign1 {
-    private static Scanner scan = new Scanner(System.in).useDelimiter("\n");
+    private static Scanner scan = new Scanner(System.in).useDelimiter("(\\n)");
 
     public static void main(String args[]){
         initialPrompt();
@@ -31,18 +31,19 @@ public class TestAssign1 {
         System.out.println("| 0. Exit Program                          |");
         System.out.println("+==========================================+");
         System.out.print("Option: ");
-        int i = scan.nextInt();
-        switch(i) {
-            case 1:
+//        int i = scan.nextInt();
+        String option = System.console().readLine();
+        switch(option) {
+            case "1":
                 createMenu();
                 break;
-            case 2:
+            case "2":
                 findMenu();
                 break;
-            case 3:
+            case "3":
                 runTestScript();
                 break;
-            case 0:
+            case "0":
                 System.exit(0);
             default:
                 mainMenu();
@@ -57,16 +58,21 @@ public class TestAssign1 {
         System.out.println("| 5. Return to previous menu               |");
         System.out.println("+==========================================+");
         System.out.print("Option: ");
-        int i = scan.nextInt();
-        switch(i){
-            case 1:
+        String option = System.console().readLine();;
+        System.out.println();
+
+        switch(option){
+            case "1":
                 createBook();
                 break;
-            case 2:
+            case "2":
                 createPatron();
                 break;
-            default:
+            case "5":
                 mainMenu();
+                break;
+            default:
+                createMenu();
                 break;
         }
     }
@@ -74,20 +80,25 @@ public class TestAssign1 {
         System.out.println("+==========================================+");
         System.out.println("| Enter option to select. Then press ENTER |");
         System.out.println("| 1. Find a Book                           |");
-        System.out.println("| 2. Find a new Patron                     |");
+        System.out.println("| 2. Find a Patron                         |");
         System.out.println("| 5. Return to previous menu               |");
         System.out.println("+==========================================+");
         System.out.print("Option: ");
-        int i = scan.nextInt();
-        switch(i){
-            case 1:
+        String option = System.console().readLine();
+        System.out.println();
+
+        switch(option){
+            case "1":
                 findBook();
                 break;
-            case 2:
+            case "2":
                 findPatron();
                 break;
-            case 5:
+            case "5":
                 mainMenu();
+                break;
+            default:
+                findMenu();
                 break;
         }
     }
@@ -101,22 +112,26 @@ public class TestAssign1 {
         System.out.println("| 5. Return to previous menu               |");
         System.out.println("+==========================================+");
         System.out.print("Option: ");
-        int i = scan.nextInt();
-        switch(i){
-            case 1:
+        String option = System.console().readLine();
+        System.out.println();
+        switch(option){
+            case "1":
                 findBooksOlderThan();
                 break;
-            case 2:
+            case "2":
                 findBooksNewerThan();
                 break;
-            case 3:
+            case "3":
                 findBooksWithTitleLike();
                 break;
-            case 4:
+            case "4":
                 findBooksWithAuthorLike();
                 break;
-            case 5:
+            case "5":
                 findMenu();
+                break;
+            default:
+                findBook();
                 break;
         }
     }
@@ -130,22 +145,27 @@ public class TestAssign1 {
         System.out.println("| 5. Return to previous menu               |");
         System.out.println("+==========================================+");
         System.out.print("Option: ");
-        int i = scan.nextInt();
-        switch(i){
-            case 1:
+        String option = System.console().readLine();
+        System.out.println();
+
+        switch(option){
+            case "1":
                 findPatronsOlderThan();
                 break;
-            case 2:
-                findPatronsNewerThan();
+            case "2":
+                findPatronsYoungerThan();
                 break;
-            case 3:
+            case "3":
                 findPatronsInZip();
                 break;
-            case 4:
+            case "4":
                 findPatronsNameLike();
                 break;
-            case 5:
+            case "5":
                 findMenu();
+                break;
+            default:
+                findPatron();
                 break;
         }
     }
@@ -155,7 +175,8 @@ public class TestAssign1 {
         System.out.println("| Return to main menu?                     |");
         System.out.println("| Any key to continue, N/n to exit         |");
         System.out.println("+==========================================+");
-        String s = scan.next();
+        String s = System.console().readLine();
+
         if (s.equals("N") || s.equals("n")) {
             System.exit(0);
         }
@@ -167,7 +188,7 @@ public class TestAssign1 {
     private static void findBooksOlderThan(){
         String input;
         System.out.print("Date to search before: ");
-        input = scan.next();
+        input = System.console().readLine();
         System.out.println("Results: ");
         BookCollection bookCol = new BookCollection();
         Vector<Book> books = bookCol.findBooksOlderThanDate(input);
@@ -179,7 +200,7 @@ public class TestAssign1 {
     private static void findBooksNewerThan(){
         String input;
         System.out.print("Date to search after: ");
-        input = scan.next();
+        input = System.console().readLine();
         System.out.println("Results: ");
         BookCollection bookCol = new BookCollection();
         Vector<Book> books = bookCol.findBooksNewerThanDate(input);
@@ -191,7 +212,7 @@ public class TestAssign1 {
     private static void findBooksWithTitleLike(){
         String input;
         System.out.print("Book Title: ");
-        input = scan.next();
+        input = System.console().readLine();
         System.out.println("Results: ");
         BookCollection bookCol = new BookCollection();
         Vector<Book> books = bookCol.findBooksWithTitleLike(input);
@@ -203,7 +224,7 @@ public class TestAssign1 {
     private static void findBooksWithAuthorLike(){
         String input;
         System.out.print("Author's Name: ");
-        input = scan.next();
+        input = System.console().readLine();
         System.out.println("Results: ");
         BookCollection bookCol = new BookCollection();
         Vector<Book> books = bookCol.findBooksWithAuthorLike(input);
@@ -217,7 +238,7 @@ public class TestAssign1 {
     private static void findPatronsOlderThan(){
         String input;
         System.out.print("Date to search before: ");
-        input = scan.next();
+        input = System.console().readLine();
         System.out.println("Results: ");
         PatronCollection patronCol = new PatronCollection();
         Vector<Patron> patrons = patronCol.findPatronsOlderThan(input);
@@ -227,10 +248,10 @@ public class TestAssign1 {
         continueMenu();
 
     }
-    private static void findPatronsNewerThan(){
+    private static void findPatronsYoungerThan(){
         String input;
         System.out.print("Date to search after: ");
-        input = scan.next();
+        input = System.console().readLine();
         System.out.println("Results: ");
         PatronCollection patronCol = new PatronCollection();
         Vector<Patron> patrons = patronCol.findPatronsYoungerThan(input);
@@ -243,7 +264,7 @@ public class TestAssign1 {
     private static void findPatronsInZip(){
         String input;
         System.out.print("ZIP Code: ");
-        input = scan.next();
+        input = System.console().readLine();
         System.out.println("Results: ");
         PatronCollection patronCol = new PatronCollection();
         Vector<Patron> patrons = patronCol.findPatronsAtZipCode(input);
@@ -256,7 +277,7 @@ public class TestAssign1 {
     private static void findPatronsNameLike(){
         String input;
         System.out.print("Patron name: ");
-        input = scan.next();
+        input = System.console().readLine();
         System.out.println("Results: ");
         PatronCollection patronCol = new PatronCollection();
         Vector<Patron> patrons = patronCol.findPatronsWithNameLike(input);
@@ -272,19 +293,20 @@ public class TestAssign1 {
         String input = "";
         Properties props = new Properties();
         System.out.print("Book Author: ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("author", input);
         System.out.print("Book Title: ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("title", input);
         System.out.print("Publication year (yyyy): ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("pubYear", input);
         System.out.print("Status (active/inactive): ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("status", input);
         Book newBook = new Book(props);
         newBook.update();
+        System.out.println("New book added!");
         continueMenu();
 
     }
@@ -293,31 +315,32 @@ public class TestAssign1 {
         String input = "";
         Properties props = new Properties();
         System.out.println("Name of Patron: ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("name", input);
         System.out.println("Address: ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("address", input);
         System.out.println("City: ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("city", input);
         System.out.println("State code: ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("stateCode", input);
         System.out.println("Zip code: ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("zip", input);
         System.out.println("Email Address: ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("email", input);
         System.out.println("Date of Birth(yyyy-mm-dd): ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("dateOfBirth", input);
         System.out.print("Status (active/inactive): ");
-        input = scan.next();
+        input = System.console().readLine();
         props.setProperty("status", input);
         Patron newPatron = new Patron(props);
         newPatron.update();
+        System.out.println("New patron added!");
         continueMenu();
 
     }
